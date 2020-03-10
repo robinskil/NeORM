93fba1b66a0b8549b0ef49242d638fd0c5299312
Robin <robin.kooyman.work@gmail.com>
2020-03-10 17:12:42 +0100

    WIP

---
+++ NeORM/NeoContext.cs
@@ -0,0 +1,35 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeORM
+{
+    public class NeoContext
+    {
+        IDriver Driver { get; }
+        
+        public NeoContext(IDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public async Task AddNode<T>(T node)
+        {
+            var session = Driver.AsyncSession();
+            try
+            {
+                await session.RunAsync("",);
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+
+
+
+    }
+}
